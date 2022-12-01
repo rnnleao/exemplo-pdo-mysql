b3b92b37bbfd70f716fa5226bd11232e16947cb7
rnnleao <renancleao@outlook.com>
2022-12-01 13:45:56 +0000

    Preparação da Base de Dados

---
+++ assets/sql/create_database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE joguinho;
+
+
+USE joguinho;
+CREATE TABLE generos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE lançamento(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    id_genero INT NOT NULL,
+    FOREIGN KEY (id_genero) REFERENCES generos(id)
+);
+
---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+INSERT INTO generos(id, nome) VALUES
+    (1, 'FPS'),
+    (2, 'ESPORTE'),
+    (3, 'TERROR'),
+    (4, 'RPG');
+
+INSERT INTO lançamento(id,  titulo,id_genero) VALUES
+    (1, 'FIFA', 2),
+    (2, 'VALORANT', 1),
+    (3, 'RE', 3),
+    (4, 'SKYRIM', 4);
